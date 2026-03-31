ed50df1fbd7469c9f36c7c8b467e0c07bd25b45e
artur <arturstasiuk153@gmail.com>
2026-03-31 07:27:01 +0200

    ww

--- public/sys/sys.js
+++ public/sys/sys.js
@@ -12,13 +12,7 @@ class SYS {
     async init() {
         console.log('Inicjalizacja systemu...');
         const info = await this.api.getInfoModules();
-        this.view.refreshStartMenu([
-            { id: 'sm-notes', icon: '📝', label: 'Notatnik', onClick: () => view.restore('win-notes') },
-            { id: 'sm-calc', icon: '🧮', label: 'Kalkulator', onClick: () => view.restore('win-calc') },
-            'separator',
-            { id: 'sm-settings', icon: '⚙️', label: 'Ustawienia', onClick: () => alert('Ustawienia') },
-            { id: 'sm-off', icon: '⏻', label: 'Wyłącz', disabled: true }
-        ]);
+
 
 
 
