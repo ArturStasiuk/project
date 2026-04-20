18d60c7848cb21e09c1fa83861543bc43fa31297
artur <arturstasiuk153@gmail.com>
2026-04-20 08:22:58 +0200

    poprawiono odczyt dla meniu

--- private/tools/admin_system/method.js
+++ private/tools/admin_system/method.js
@@ -13,19 +13,21 @@ class METHOD{
         const odp = await this.parent.api.send({ modules: 'modules_company', method:'getAllCompanyData', param: {data} });
         return odp.data;
     }
+
     async accessMenu_Window_ZarzadzajFirmami() {
         const odp = await this.accessTable();
         return {
-            "read":  odp.access_table.read_record,
-            "create": odp.access_table.add_record,
-            "update": odp.access_table.update_record,
-            "delete": odp.access_table.delete_record
+            "read": !!odp.read_record,
+            "create": !!odp.add_record,
+            "update": !!odp.update_record,
+            "delete": !!odp.delete_record
         };
     }
 
     /** sprawdza dostęp do tabeli company */
     async accessTable() {
         const odp = await this.parent.api.send({ method:'gestAccessTtables', param: { tables: 'company' } });
+
         return odp
     }
 }
