29d23d789f38da67f5623ebd5763f12a4d15ab73
ArturStasiuk <arturstasiuk153@gmail.com>
2026-04-26 06:41:35 +0200

    dd

--- private/tools/admin_system/edytuj.js
+++ private/tools/admin_system/edytuj.js
@@ -1,6 +1,6 @@
 class EDYCJA_FIRMY  {
     constructor(parent) {
-        this.parent = parent;
+        this.admin = parent;
         this.idWindow = 'window-edytuj-firme';
         this.idMeniu = 'meniu-edytuj-firme';
      //   this.idCompany = id_company;
@@ -10,11 +10,7 @@ class EDYCJA_FIRMY  {
     async open(id_company) {
      console.log('Otwieranie okna edycji firmy o ID:', id_company);
      // sprawdzenie czy firma istnieje
-     const firmaData = await this.parent.getCompanyById(id_company);
-        if (!firmaData) {
-            alert('Nie można znaleźć danych firmy. Edycja niemożliwa.');
-            return;
-        }   
+  
     }
 
 
